8c7c700d4c223de4f3ae71530720a2d7589f3eae
AbhitheTester <abhishek964027@gmail.com>
2026-04-07 11:38:06 +0530

    Playwright config change in report and headless true

--- playwright.config.js
+++ playwright.config.js
@@ -1,29 +1,24 @@
 // @ts-check
-import { chromium, defineConfig, devices } from '@playwright/test';
+import { defineConfig } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
- * @see https://playwright.dev/docs/test-configuration
- */
 export default defineConfig({
   testDir: './tests',
- timeout:40*1000,
- expect:{
-  timeout:30*1000
- },
- reporter:'html',
-  use: {
-   browserName:"chromium",
-   headless:false
+
+  timeout: 40 * 1000,
+
+  expect: {
+    timeout: 5 * 1000, // 5s is safer than 3s for CI
   },
 
+  reporter: [
+    ['html', { outputFolder: 'playwright-report', open: 'never' }]
+  ],
 
+  use: {
+    browserName: 'chromium',
+    headless: true, // IMPORTANT for Jenkins
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'on-first-retry',
+  },
 });
-
